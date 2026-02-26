0789b53fd5035a179090f9698df2939d49abcb5d
chenkezhe <chenkezhe@corp.netease.com>
2026-02-26 19:00:10 +0800

    时间用默认的 now, 不用 tzinfo

--- vnpy_scripttrader/engine.py
+++ vnpy_scripttrader/engine.py
@@ -355,7 +355,7 @@ class ScriptEngine(BaseEngine):
         if self.is_testing:
             ret = self.test_tick.datetime
         else:
-            ret = datetime.now(tz=DB_TZ)
+            ret = datetime.now()
         return ret
 
     def get_account_available(self) -> float:
